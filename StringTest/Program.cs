1df4163a9230b2c7f3cde258ea5409c43e6f1a99
unknown <qiong_qiong@163.com>
2018-01-11 14:57:17 +0800

    first commit

---
+++ StringTest/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringTest
+{
+    /// <summary>
+    /// 面试:string是引用类型还是值类型?
+    /// 回答:引用类型.
+    /// 面试：string为引用类型?那如下,调用SetStr方法为何输出结果为何不为"bbb"?
+    /// 回答：string是一个特殊的引用类型.
+    /// 面试:需要大量拼接字符串需要怎么优化?
+    /// 回答:StringBuilder.
+    /// </summary>
+    class Program
+    {
+        public void SetStr(string str)
+        {
+            str = "bbb";
+        }
+
+        static void Main(string[] args)
+        {
+            string str = "aaa";
+            Program p = new Program();
+            p.SetStr(str);
+            Console.WriteLine(str); //输出结果:aaa
+
+            /*string是一个特殊的引用类型,用法跟值类型相似;string是只读的引用类型，修改string对象会创建新的实例*/
+
+            str = "ccc";
+            Test t = new Test();
+            t.mystr = str;
+            Console.WriteLine(str); //输出结果:ccc
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ StringTest/StringBuilderTest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringTest
+{
+    /// <summary>
+    /// 面试:需要大量拼接字符串需要怎么优化?
+    /// 回答:StringBuilder.
+    /// </summary>
+    class StringBuilderTest
+    {
+        static void Main1(string[] args)
+        {
+            StringBuilder sb = new StringBuilder();
+            DateTime dt2 = DateTime.Now;
+            for (int i = 0; i < 200000; i++)
+            {
+                sb.Append("aaaaa");
+            }
+            string str2 = sb.ToString();
+            Console.WriteLine((DateTime.Now - dt2).TotalMilliseconds);
+
+
+            DateTime dt = DateTime.Now;
+            string str = "";
+            for (int i = 0; i < 200000; i++)
+            {
+                str += "bbbbbbb";
+            }
+            Console.WriteLine((DateTime.Now - dt).TotalMilliseconds);
+
+            Console.ReadLine();
+        }
+    }
+}
